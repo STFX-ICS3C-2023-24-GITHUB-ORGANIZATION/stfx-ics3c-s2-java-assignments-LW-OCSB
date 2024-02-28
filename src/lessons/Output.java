e72bf721c604302bd9e616168d674fe176835d0c
e28166 <e28166@FXH-0015840124>
2024-02-28 11:57:00 -0500

    s

---
+++ src/lessons/Output.java
@@ -0,0 +1,14 @@
+package lessons;
+
+public class Output {
+
+	public static void main(String[] args) {
+
+		// variables
+		int grade = 100;
+		double money = 9.99;
+		
+
+	}
+
+}
--- src/lessons/Variables.java
+++ src/lessons/Variables.java
@@ -4,9 +4,22 @@ public class Variables {
 
 	public static void main(String[] args) {
 
-		// 10 integers
-		int myIntegers = 13;
+		// integers
+		int myIntegers1 = 13;
+		int myIntegers2 = 22;
 		
+		// doubles
+		double myDouble1 = 13.3;
+		
+		// booleans
+		boolean myBoolean1 = true;
+		boolean myBoolean2 = false;
+		
+		// chars
+		char myChar1 = 'a';
+		
+		// Strings
+		String myString1 = "hello";
 		
 
 	}
