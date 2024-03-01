b73087dc233677142734a11f415e7a51f17faf55
e28166 <e28166@FXH-0015840124>
2024-03-01 12:25:56 -0500

    s

--- src/lessons/CandyMachine.java
+++ src/lessons/CandyMachine.java
@@ -1,11 +1,41 @@
 package lessons;
+import java.util.Scanner;
 
 public class CandyMachine {
 
 	public static void main(String[] args) {
 		
-		// variables (including constants)
-
+		// variables
+		final double TAX = 0.13;
+		Scanner inputGetter = new Scanner(System.in);
+		double candyCost = 0.0;
+		double candyTax = 0.0;
+		double grandTotal = 0.0;
+		
+		// output
+		System.out.println("Welcome to Candy Machine!\n");
+		System.out.println("Choices are (without tax): \n");
+		System.out.println("1. Potato Chips $2.00");
+		System.out.println("2. Chocolate Bar $1.50");
+		System.out.println("3. Rice Crispies $2.55\n");
+		System.out.println("Your choice (just enter cost): ");
+		
+		// get cost
+		candyCost = inputGetter.nextDouble();
+		
+		// calculate tax
+		candyTax = candyCost * TAX;
+		System.out.println("Tax: $" + candyTax);
+		
+		// print subtotal
+		System.out.println("The subtotal is: $" + candyCost);
+		
+        // calculate grand total
+		grandTotal = candyCost + candyTax;
+		
+		// print grand total
+		System.out.println("The grand total is: $" + grandTotal);
+		
 	}
 
 }
