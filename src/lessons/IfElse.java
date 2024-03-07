5a24b61454978b517c4032d5c2f0379ad7a98265
e28166 <e28166@FXH-0015840124>
2024-03-07 12:27:02 -0500

    s

---
+++ src/lessons/IfElse.java
@@ -0,0 +1,25 @@
+package lessons;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+
+		// variables
+		int myNumber1 = 95;
+		int myNumber2 = 76;
+		int myNumber3 = 95;
+		
+		// Comparisons:  >, <, >=, <=, ==, !=
+		if (myNumber1 != myNumber3)  {
+			
+			System.out.println("True.");
+		}
+		
+		else  {
+			System.out.println("False.");
+		}
+		
+		
+	}
+
+}
